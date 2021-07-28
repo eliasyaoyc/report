3eb0fc26b2482e28adfa291abd982ed29ad2fb4a
Elias.Yao <siran0611@gmail.com>
2021-07-28 22:44:31 +0800

    runtime.

--- kits/report-kit/src/main/java/yyc/open/framework/microants/components/kit/report/handler/ChartHandler.java
+++ kits/report-kit/src/main/java/yyc/open/framework/microants/components/kit/report/handler/ChartHandler.java
@@ -3,12 +3,12 @@ package yyc.open.framework.microants.components.kit.report.handler;
 import yyc.open.framework.microants.components.kit.report.event.ReportEvent;
 
 /**
- * {@link ImageHandler}
+ * {@link ChartHandler}
  *
  * @author <a href="mailto:siran0611@gmail.com">Elias.Yao</a>
  * @version ${project.version} - 2021/7/28
  */
-public class ImageHandler implements Handler{
+public class ChartHandler implements Handler{
     @Override
     public void onHandle(ReportEvent event) {
 
