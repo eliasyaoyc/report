699ca15b4b747c75375e9352af77d70951cab824
Elias.Yao <siran0611@gmail.com>
2021-07-31 12:32:27 +0800

    adjust project structure.

--- enhance/elasticsearch-orm-enhance/src/main/java/yyc/open/framework/microants/components/enhance/elasticsearch/exceptions/BulkFailureException.java
+++ enhance/elasticsearch-orm-enhance/src/main/java/yyc/open/framework/microants/components/enhance/elasticsearch/exceptions/BulkFailureException.java
@@ -1,4 +1,4 @@
-package yyc.open.framework.microants.components.starter.es.exceptions;
+package yyc.open.framework.microants.components.enhance.elasticsearch.exceptions;
 
 import java.util.Map;
 
@@ -8,7 +8,7 @@ import java.util.Map;
  * @author <a href="mailto:siran0611@gmail.com">Elias.Yao</a>
  * @version ${project.version} - 2021/7/23
  */
-public class BulkFailureException extends ElasticsearchException{
+public class BulkFailureException extends ElasticsearchException {
     private static final long serialVersionUID = 5241692221150614145L;
 
     private final Map<String,String> failedDocuments;
