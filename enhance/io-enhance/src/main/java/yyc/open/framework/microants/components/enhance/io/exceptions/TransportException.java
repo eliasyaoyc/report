03a437b5ecb64755ee5061714399783390fd5a0b
Elias.Yao <siran0611@gmail.com>
2021-07-31 11:12:21 +0800

    adjust project structure

---
+++ enhance/io-enhance/src/main/java/yyc/open/framework/microants/components/enhance/io/exceptions/TransportException.java
@@ -0,0 +1,11 @@
+package yyc.open.framework.microants.components.enhance.io.exceptions;
+
+/**
+ * {@link TransportException}
+ *
+ * @author <a href="mailto:siran0611@gmail.com">Elias.Yao</a>
+ * @version ${project.version} - 2021/7/31
+ */
+public class TransportException extends RuntimeException {
+    private static final long serialVersionUID = -2331456878657481486L;
+}
