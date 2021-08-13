9be05464c22382681ea1279d9bcef53c91e5d4dc
Elias.Yao <siran0611@gmail.com>
2021-08-13 13:25:54 +0800

    fixbug

--- starters/report-starter/src/main/java/yyc/open/framework/microants/components/starter/report/autoconfigure/MicroantsReportAutoConfiguration.java
+++ starters/report-starter/src/main/java/yyc/open/framework/microants/components/starter/report/autoconfigure/MicroantsReportAutoConfiguration.java
@@ -1,18 +1,16 @@
 package yyc.open.framework.microants.components.starter.report.autoconfigure;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
+import yyc.open.framework.microants.components.kit.common.BeansKit;
 import yyc.open.framework.microants.components.kit.report.Report;
 import yyc.open.framework.microants.components.kit.report.ReportBuilder;
 import yyc.open.framework.microants.components.kit.report.ReportConfig;
 import yyc.open.framework.microants.components.kit.report.ReportRuntime;
-
-import static yyc.open.framework.microants.components.starter.report.autoconfigure.MicroantsReportProperties.MICROANTS_FILE;
 
 /**
  * {@link MicroantsReportAutoConfiguration}
@@ -23,29 +21,17 @@ import static yyc.open.framework.microants.components.starter.report.autoconfigu
 @Configuration
 @EnableConfigurationProperties(MicroantsReportProperties.class)
 public class MicroantsReportAutoConfiguration {
-
-    @Bean
-    @Primary
-    @ConditionalOnClass(MicroantsReportProperties.class)
-    @ConditionalOnProperty(prefix = MICROANTS_FILE, value = "echart", havingValue = "enabled")
-    public ReportRuntime reportEchartsRuntime() {
-        ReportRuntime runtime = new ReportRuntime();
-        runtime.start(true);
-        return runtime;
-    }
-
-    @Bean
-    @ConditionalOnMissingBean(value = ReportRuntime.class)
-    public ReportRuntime reportRuntime() {
-        ReportRuntime runtime = new ReportRuntime();
-        runtime.start(false);
-        return runtime;
-    }
+    private static final Logger LOGGER = LoggerFactory.getLogger(MicroantsReportAutoConfiguration.class);
 
     @Bean
     @ConditionalOnClass(MicroantsReportProperties.class)
     public Report report(MicroantsReportProperties properties) {
-        ReportConfig reportConfig = properties.getReportConfig();
+        ReportRuntime reportRuntime = new ReportRuntime();
+        reportRuntime.start(properties.getEchart());
+        LOGGER.info("[ReportRuntime] start success.");
+
+        ReportConfig reportConfig = new ReportConfig();
+        BeansKit.copyProperties(properties, reportConfig);
         return ReportBuilder.builder().config(reportConfig).build();
     }
 }
--- starters/report-starter/src/main/java/yyc/open/framework/microants/components/starter/report/autoconfigure/MicroantsReportProperties.java
+++ starters/report-starter/src/main/java/yyc/open/framework/microants/components/starter/report/autoconfigure/MicroantsReportProperties.java
@@ -1,7 +1,10 @@
 package yyc.open.framework.microants.components.starter.report.autoconfigure;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import yyc.open.framework.microants.components.kit.report.ReportConfig;
+
+import java.util.List;
 
 import static yyc.open.framework.microants.components.starter.report.autoconfigure.MicroantsReportProperties.MICROANTS_FILE;
 
@@ -11,17 +14,18 @@ import static yyc.open.framework.microants.components.starter.report.autoconfigu
  * @author <a href="mailto:siran0611@gmail.com">Elias.Yao</a>
  * @version ${project.version} - 2021/7/28
  */
+@Getter
+@Setter
 @ConfigurationProperties(prefix = MICROANTS_FILE)
 public class MicroantsReportProperties {
     protected static final String MICROANTS_FILE = "microants.report";
 
-    private ReportConfig reportConfig;
-
-    public ReportConfig getReportConfig() {
-        return reportConfig;
-    }
-
-    public void setReportConfig(ReportConfig reportConfig) {
-        this.reportConfig = reportConfig;
-    }
+    private Boolean echart = false;
+    private List<String> colors;
+    private String watermark;
+    private Boolean horizontal;
+    private Integer port;
+    private Boolean parallel;
+    private Integer maxTaskNum;
+    private String outputPath;
 }
