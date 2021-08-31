0547e824b56cc56ee2be8fad781343add0ced4ec
Elias.Yao <siran0611@gmail.com>
2021-08-31 17:48:14 +0800

    update audit entity.

--- kits/audit-kit/src/main/java/yyc/open/framework/microants/components/kit/audit/AuditEntity.java
+++ kits/audit-kit/src/main/java/yyc/open/framework/microants/components/kit/audit/AuditEntity.java
@@ -1,8 +1,47 @@
 package yyc.open.framework.microants.components.kit.audit;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 /**
  * @author Elias (siran0611@gmail.com)
  */
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class AuditEntity {
 
+	private String userName;
+	private String userRole;
+	private String ip;
+	private String action;
+	private String content;
+	private Boolean isSuccess;
+	private Long createTime;
+	private String module;
+
+	public static AuditEntity success(
+			String userName,
+			String userRole,
+			String ip,
+			String action,
+			String content,
+			Long createTime,
+			String module) {
+		return new AuditEntity(userName, userRole, ip, action, content, true, createTime, module);
+	}
+
+	public static AuditEntity failure(
+			String userName,
+			String userRole,
+			String ip,
+			String action,
+			String content,
+			Long createTime,
+			String module) {
+		return new AuditEntity(userName, userRole, ip, action, content, false, createTime, module);
+	}
 }
--- kits/audit-kit/src/test/java/yyc/open/framework/microants/components/kit/audit/AuditTest.java
+++ kits/audit-kit/src/test/java/yyc/open/framework/microants/components/kit/audit/AuditTest.java
@@ -1,6 +1,5 @@
 package yyc.open.framework.microants.components.kit.audit;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 import org.junit.Test;
@@ -20,16 +19,20 @@ public class AuditTest {
 
 	@Test
 	public void testAudit() {
-		audit.publishEvent(new TestEntity("yyc", 12));
+		audit.publishEvent(new TestEntity("yyc", "sys", "1.1.1.1", "添加", "添加用户", true, 121212121L, ""));
 	}
 
 
 	@Getter
 	@Setter
-	@AllArgsConstructor
 	class TestEntity extends AuditEntity {
 
 		private String name;
 		private Integer age;
+
+		public TestEntity(String userName, String userRole, String ip, String action, String content, Boolean isSuccess,
+				Long createTime, String module) {
+			super(userName, userRole, ip, action, content, isSuccess, createTime, module);
+		}
 	}
 }
